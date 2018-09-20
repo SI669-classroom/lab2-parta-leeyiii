3fc4855f11162b6ae3a8955dece59acc0b36493d
Bonnie <yilee@umich.edu>
2018-09-20 14:24:03 -0400

    flip

--- lab2a-promise.js
+++ lab2a-promise.js
@@ -10,14 +10,126 @@ function flip() {
 
 //// Add code here that will "flip" the coin ten times and write the 
 //// result to the console (e.g. "Heads" or "Tails" for each flip).
+function flipCoin() {
+    return (Math.random() > 0.5);
+}
+
+function heads() {
+    console.log("Heads!");
+}
+
+function tails() {
+    console.log("Tails!");
+}
+
+// This is the function that the Promise will invoke when it's "go" time.
+// The parameters are *functions* that will be called depending on what happens
+function resolvePromise(resolve, reject) { 
+    if (flipCoin()) resolve();
+    else reject();
+}   
+
+let p = new Promise(resolvePromise); // this sets the Promise up, but nothing happens yet
+
+for (let i = 0; i < 10; i++) {
+    p.then(heads, tails);
+    p = new Promise(resolvePromise); // what happens if you remove this?
+}
 
 
 
+/*
 
 ///// Problem 2 - More fun...
 
 function countBig(bignum) {
     // Add code here that returns a Promise that will resolve after it has counted to bignum
+    ///////////////////////////////////////////////////////////////////////
+
+// "Normal" functions
+}
+
+function squish(word) {
+    if (word.length < 2) {
+        return word;
+    }
+    return word[0] + word[word.length-1];
+}
+
+console.log("Squished Hello is " + squish("Hello"));
+
+///////////////////////////////////////////////////////////////////////
+// Functions as variables
+function takeOutTheMiddle(word) {
+    if (word.length < 3) {
+        return word;
+    } else {
+        return word.slice(1, word.length - 1 );
+    }
+}
+
+let gut = takeOutTheMiddle;
+console.log("Gutted Hello is " + gut("Hello"));
+
+///////////////////////////////////////////////////////////////////////
+// Assigning variables to anonymous functions
+let penultimate = function(word) {
+    if (word.length < 2) {
+        return word;
+    } else {
+        return word[word.length - 2];
+    }
+}
+console.log("Penultimate Hello is " + penultimate("Hello"));
+
+///////////////////////////////////////////////////////////////////////
+//Functions as parameters
+function compareLast(word1, word2) {
+    a = word1[word1.length - 1];
+    b = word2[word2.length - 1]; 
+    if (a > b) {
+        return 1;
+    } else if (a < b) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
+
+someWords = [
+    'apricot',
+    'mango',
+    'banana',
+    'grapefruit',
+    'kiwi',
+    'cherry'
+]
+
+someWords.sort();
+console.log("Sorted 'normally': " + someWords);
+
+someWords.sort(compareLast);
+console.log("Sorted by last letter: " + someWords);
+
+///////////////////////////////////////////////////////////////////////
+//Anonymous functions as parameters
+someWords.sort(function(a, b) { return a.length - b.length});
+console.log("Sorted by word length: " + someWords);
+
+///////////////////////////////////////////////////////////////////////
+//Arrow functions - a concise way to define anonymous functions
+someWords.sort((a, b) => {
+    return (
+        a.split('').sort().reverse()[0].charCodeAt(0) - 
+        b.split('').sort().reverse()[0].charCodeAt(0));
+});
+console.log("Sorted by 'biggest' letter: " + someWords);
+
+
+
+
+
+
 }
 
 
@@ -28,3 +140,5 @@ countBig(1000000000).then(()=> {
 }, () => {
     console.log("A problem occurred while trying to count to " + bignum);
 })
+
+*/
